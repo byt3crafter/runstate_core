65c532369955eb2d8e90c1065db78523c7615ada
Yousef <youssefrestom@gmail.com>
2024-05-10 05:22:23 +0300

    feat: Highlight the rules prices of the items based on if there is a price change

--- impex/impex/doctype/price_change/price_change.js
+++ impex/impex/doctype/price_change/price_change.js
@@ -1,8 +1,20 @@
 // Copyright (c) 2024, Yousef Restom and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Price Change', {
-	// refresh: function(frm) {
-
-	// }
+frappe.ui.form.on("Price Change", {
+  refresh: function (frm, cdt, cdn) {
+    cur_frm.fields_dict.rule_prices.$wrapper
+      .find(".grid-body .rows")
+      .find(".grid-row")
+      .each(function (i, item) {
+        let d =
+          locals[cur_frm.fields_dict["rule_prices"].grid.doctype][
+            $(item).attr("data-name")
+          ];
+        if (d.new_rate != d.last_rate) {
+          $(item).find(".grid-static-col").css({ color: "#FF0000" });
+        }
+      });
+    frm.refresh_field("rule_prices");
+  },
 });
