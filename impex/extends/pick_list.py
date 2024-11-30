1386bf1fe617b51cb4c51bed36e66d1e429c095a
Yousef <youssefrestom@gmail.com>
2024-11-30 15:42:42 +0400

    feat: Bin Location multi items

--- impex/extends/pick_list.py
+++ impex/extends/pick_list.py
@@ -8,9 +8,16 @@ def validate(doc, method):
 def set_bin_location(doc, method):
     if doc.get("locations") and len(doc.locations) > 0:
         for item in doc.locations:
-            if item.item_code and item.warehouse:
-                item.custom_bin_location = frappe.get_cached_value(
-                    "Bin Location",
-                    {"item_code": item.item_code, "warehouse": item.warehouse},
-                    "name",
+            if item.item_code and item.warehouse and not item.custom_bin_location:
+                all_item_bins = frappe.db.sql(
+                    """
+                    SELECT BL.name 
+                    FROM `tabBin Location` BL  
+                    LEFT JOIN `tabBin Location Item` BLI on BL.name = BLI.parent
+                    WHERE BL.warehouse=%s
+                    AND BLI.item_code=%s
+                    """,
+                    (item.warehouse, item.item_code),
                 )
+                if all_item_bins:
+                    item.custom_bin_location = all_item_bins[0][0]
