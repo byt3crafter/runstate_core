b9ab0a0c2781a397a16c7a4e73544f4b75ac2a4f
Aisenyi <malisa.aisenyi@gmail.com>
2025-03-14 13:51:48 +0300

    fix: Implement invoice renaming only for POS invoices

--- impex/extends/sales_invoice.py
+++ impex/extends/sales_invoice.py
@@ -3,11 +3,19 @@ from frappe.model.naming import getseries, parse_naming_series
 from datetime import datetime
 
 def autoname(doc, method):
-	naming_series = f"SINV-DRAFT"
-	doc.name = f"SINV-DRAFT-{getseries(naming_series, 4)}"
+	if doc.is_pos:
+		naming_series = f"SINV-DRAFT"
+		doc.name = f"SINV-DRAFT-{getseries(naming_series, 4)}"
+	else:
+		naming_series = parse_naming_series(doc.naming_series, "Sales Invoice", doc)
+		if doc.naming_series.endswith('#'):
+			doc.name = naming_series
+		else:
+			doc.name = f"{naming_series}{getseries(naming_series, 4)}"
 
 def on_submit(doc, method):
-	rename_invoice(doc)
+	if doc.is_pos:
+		rename_invoice(doc)
 
 def rename_invoice(doc):
 	naming_series = parse_naming_series(doc.naming_series, "Sales Invoice", doc)
