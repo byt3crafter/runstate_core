29bbf7321aadbc0c45c2bd873658a93627e04aaa
Aisenyi <malisa.aisenyi@gmail.com>
2025-05-08 16:13:35 +0300

    chore: Modified price change doctype to add changed prices table
    - This will help solve the confusion on which prices in the price rule table will actually be changed

--- impex/impex/doctype/price_change/price_change.js
+++ impex/impex/doctype/price_change/price_change.js
@@ -2,36 +2,36 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Price Change", {
-  refresh: function (frm, cdt, cdn) {
-    cur_frm.fields_dict.rule_prices.$wrapper
-      .find(".grid-body .rows")
-      .find(".grid-row")
-      .each(function (i, item) {
-        let d =
-          locals[cur_frm.fields_dict["rule_prices"].grid.doctype][
-            $(item).attr("data-name")
-          ];
-        if (d.new_rate != d.last_rate) {
-          setTimeout(() => {
-            $(item).find(".grid-static-col").css({ color: "#FF0000" });
-            $(item).find("a").attr("style", "color: #FF0000");
-          }, 100);
-        }
-        if (d.price_list.includes("Selling")) {
-          setTimeout(() => {
-            $(item)
-              .find("[data-name='" + d.price_list + "']")
-              .css({ color: "#0000FF" });
-          }, 100);
-        }
-        if (d.price_list.includes("Buying")) {
-          setTimeout(() => {
-            $(item)
-              .find("[data-name='" + d.price_list + "']")
-              .css({ color: "#008000" });
-          }, 100);
-        }
-      });
-    frm.refresh_field("rule_prices");
-  },
+//   refresh: function (frm, cdt, cdn) {
+//     cur_frm.fields_dict.rule_prices.$wrapper
+//       .find(".grid-body .rows")
+//       .find(".grid-row")
+//       .each(function (i, item) {
+//         let d =
+//           locals[cur_frm.fields_dict["rule_prices"].grid.doctype][
+//             $(item).attr("data-name")
+//           ];
+//         if (d.new_rate != d.last_rate) {
+//           setTimeout(() => {
+//             $(item).find(".grid-static-col").css({ color: "#FF0000" });
+//             $(item).find("a").attr("style", "color: #FF0000");
+//           }, 100);
+//         }
+//         if (d.price_list.includes("Selling")) {
+//           setTimeout(() => {
+//             $(item)
+//               .find("[data-name='" + d.price_list + "']")
+//               .css({ color: "#0000FF" });
+//           }, 100);
+//         }
+//         if (d.price_list.includes("Buying")) {
+//           setTimeout(() => {
+//             $(item)
+//               .find("[data-name='" + d.price_list + "']")
+//               .css({ color: "#008000" });
+//           }, 100);
+//         }
+//       });
+//     frm.refresh_field("rule_prices");
+//   },
 });
