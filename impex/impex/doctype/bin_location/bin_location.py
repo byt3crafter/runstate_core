b772da93a427cf4012a078613554b1840df6e2a2
Aisenyi <malisa.aisenyi@gmail.com>
2025-05-13 22:36:14 +0300

    chore: Changed bin location naming

--- impex/impex/doctype/bin_location/bin_location.py
+++ impex/impex/doctype/bin_location/bin_location.py
@@ -1,8 +1,14 @@
 # Copyright (c) 2024, Yousef Restom and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class BinLocation(Document):
-	pass
+	def autoname(self):
+		"""Set name as warehouse-location"""
+		if self.warehouse and self.location:
+			warehouse_name = frappe.db.get_value("Warehouse", self.warehouse, "warehouse_name")
+			self.name = f"{self.location} - {warehouse_name}"
+		else:
+			frappe.throw("Warehouse and Location are required for naming the Bin Location")
