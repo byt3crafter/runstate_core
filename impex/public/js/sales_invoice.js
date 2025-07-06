c16bdb07650dd53f0219ef7a423bdee25c85b645
Aisenyi <malisa.aisenyi@gmail.com>
2025-07-06 23:23:54 +0300

    chore: Uncheck Update Stock in Sales Invoice if created from Delivery Note

--- impex/public/js/sales_invoice.js
+++ impex/public/js/sales_invoice.js
@@ -1,6 +1,7 @@
 frappe.ui.form.on("Sales Invoice", {
 	onload: function(frm) {
-		if(frm.doc.__islocal){
+		if(frm.doc.__islocal && !(frm.doc.items && frm.doc.items.length > 0 
+			&& frm.doc.items[0].delivery_note && frm.doc.items[0].delivery_note != "")){
 			frm.set_value("update_stock", 1);
 		}
 	}
